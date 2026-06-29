9c16333d1b631ee4067be7b3bc169957eca6c169
Mouryaveer <mouryaveerast@gmail.com>
2026-06-29 14:36:27 +0530

    Update changes

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -111,22 +111,6 @@ export function HeroSection() {
             </button>
           ))}
         </motion.div>
-
-        {/* Scroll indicator */}
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.6, delay: 1 }}
-          className="absolute bottom-8 left-1/2 -translate-x-1/2"
-        >
-          <motion.div
-            animate={{ y: [0, 8, 0] }}
-            transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut" }}
-            className="w-5 h-8 rounded-full border-2 border-[#D4CFC3] flex justify-center pt-1.5"
-          >
-            <div className="w-1 h-2 rounded-full bg-[#C89A4B]" />
-          </motion.div>
-        </motion.div>
       </div>
     </section>
   );
